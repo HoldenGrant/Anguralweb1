19f62ef2f9b7fee9a4c2ec5e9f056f674f0eba5c
holden_grant <holdengrantcausing@gmail.com>
2017-07-11 06:13:42 +0800

    nav title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app test';
+  title = 'Angular 4';
   title_test = 'test title';
   obj = {
       id: "1",
